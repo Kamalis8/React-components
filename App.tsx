b319e1e1e026b5d5fdeba6d94ddcb1831f9c437c
Kamalis8 <kamali.s@optisolbusiness.com>
2023-10-12 16:50:22 +0530

    Your commit message

--- App.tsx
+++ App.tsx
@@ -8,116 +8,75 @@
 import React from 'react';
 import {Alert, Button, Image, Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 
-const LotsOfStyles = () => {
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
+
+      <SafeAreaView>
       <View>
-      <Text style= {styles.text}>Text styles</Text>
+      <Text style= {styles.heading}>Cards</Text>
       </View>
 
-      <TouchableOpacity style={styles.touch} onPress ={ () => Alert.alert('saved')}><Text style ={styles.touchtext}>click to save </Text></TouchableOpacity>
+      <ScrollView horizontal= {true}>
+        <Text style= {[styles.cards,styles.cardone]}>blue</Text>
+        <Text style= {[styles.cards,styles.cardtwo]}>yellow</Text>
+        <Text style= {[styles.cards,styles.cardthree]}>green</Text>
+        <Text style= {[styles.cards,styles.cardfour]}>sandle</Text>
+        <Text style= {[styles.cards,styles.cardfive]}>violet </Text>
+        </ScrollView>
 
-      <TouchableHighlight style = {styles.touchhigh} onLongPress={ () => Alert.alert ('entered')}><Text style={styles.touchtext}>Click to enter </Text></TouchableHighlight>
-      <View style={styles.imgview}>
+        <Text style = {styles.heading}>Pictures</Text>
 
-      <Image style ={styles.img} source ={{ uri:'https://reactnative.dev/img/tiny_logo.png'}}/>
-      </View>
-
-      <KeyboardAvoidingView
-      behavior="padding"
-      style={styles.safe}>
-      
-        <View>
-      <TextInput placeholder="Username" style={styles.textinput} />
-      <Button title="Submit" onPress={() => null} />
-      </View>
-     </KeyboardAvoidingView>
-    
-    
-
-     <Text style = {styles.textpara} >Architecture Overview is intended to share conceptual overview of how React Native's internals work. The intended audience includes library authors and core contributors. If you are an app developer,it is not a requirement to be familiar with this material to be effective with React Native.You can still benefit from the overview as it will give you insights into how React Native works under the hood.Feel free to share your feedback on the discussion inside the working group for this section.</Text>
-     
-     
-     </ScrollView>
-      
-    </SafeAreaView>
-
-    
+        <ScrollView  horizontal={true}>
+          <Image style= {styles.picture} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQivPMPcek0RAaIbjP6bcRCJXq2pIu4tdjS0Q&usqp=CAU'}}/>
+          
+          <Image style= {styles.picture} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRjGFiGndwGxSPKXSY9Pj8xG7fE950kXyJOA&usqp=CAU'}}/>
+          <Image style= {styles.picture} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI7LMbw4GQH1bLXqB9etxrgDcfaA7BwZKLsg&usqp=CAU'}}/>
+          <Image style= {styles.picture} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKfiSjIN7odlEvQ8579yPbU-hRWaV2-_lIMRuCJAfilUfGuP6i0Sf3uJ0XzP4oSu6pf4U&usqp=CAU'}}/>
+          <Image style= {styles.picture} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLUTfwMlwd21mdK2tMhS4O8r_C_PTxH5mPYA&usqp=CAU'}}/>
+        </ScrollView>
+     </SafeAreaView> 
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  
+  heading:{
+    fontSize:20,
+    fontWeight:'bold',
+    
+  },
+  cards:{
     flex:1,
-    backgroundColor:'grey',
-  },
-   
-  con:{
-
-  },
-  text:{
-    textAlign:'center',
-    color:'black',
-    fontWeight:'bold',
-    backgroundColor:'yellow',
-    borderWidth:2,
-    borderColor:'red',
-    padding:20,
-    margin:10,
-    fontSize:20,
-},
-touch:{
-  alignItems:'center',
-  backgroundColor:'pink',
-  padding:10,
-},
-  touchtext:{
-    textAlign:'center',
-    color:'black',
-    fontWeight:'bold',
-    padding:20,
-    margin:10,
-    fontSize:20,
-  },
-  touchhigh:{
-    alignItems:'flex-end',
-   backgroundColor:'#ff7f50',
-   padding:10,
-  },
-   textinput:{
-    height:40,
-    borderColor: '#000000',
-    borderBottomWidth: 1,
-    marginBottom: 36,
-  },
-  safe:{
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 16,
-  },
-  img:{
+    alignItems:'center',
+    justifyContent:'center',
     width:100,
     height:100,
-    resizeMode: 'contain',
+    padding:20,
+    margin:8,
   },
-
-  imgview:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-
+ picture:{
+  width:100,
+  height:100,
+  padding:20,
+  margin:8,
+  borderRadius:20,
+ },
+  cardone:{
+    backgroundColor:'#87ceeb',
   },
-  textpara:{
-    fontSize:40,
-    color:'#8b0000',
+  cardtwo:{
+    backgroundColor:'#ffff00',
   },
-   
+  cardthree:{
+    backgroundColor:'green',
   },
-  
-  
-  
-
+  cardfour:{
+    backgroundColor:'#f5deb3',
+  },
+  cardfive:{
+    backgroundColor:'#ee82ee',
+  },
+  },
 );
 
-export default LotsOfStyles;
+export default App;
