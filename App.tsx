560e207761d3b9707c995f4eda0157f2efabddb3
Kamalis8 <kamali.s@optisolbusiness.com>
2023-10-17 12:51:55 +0530

    hooks: usestate

--- App.tsx
+++ App.tsx
@@ -5,110 +5,30 @@
  * @format
  */
 
-import React from 'react';
-import {Alert, Button, Image, Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View, Linking} from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Pressable } from 'react-native';
 
-const App = () => {
- 
-  function openwebsite (websitelink:string)
-  {
-    Linking.openURL(websitelink)
-  }
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
   return (
-
-     
-
-    <ScrollView>
-    <View style = {styles.reactview}>
-    <Text style = {styles.heading}> React Native </Text>
-
-  <Text style = {styles.headview}>
-    What is React Native?
-  </Text>
-  <Text style={styles.ans} numberOfLines={2}>
-  Many different kinds of people use React Native: from advanced iOS developers to React beginners, to people getting started programming for the first time in their career. These docs were written for all learners, no matter their experience level or background.
-  </Text>
-
-  <View style = {styles.imgview} >
-    <Image  style = {styles.Image}source={{uri:'https://images.pexels.com/photos/5077039/pexels-photo-5077039.jpeg?auto=compress&cs=tinysrgb&w=600'}}></Image>
-  </View>
-
-  
-
-  <View style={styles.touch} >
-  <TouchableOpacity  onPress={()=> openwebsite('https://reactnative.dev/docs/getting-started')}>
-    <Text style= {styles.linktext}> Read more </Text>
-    </TouchableOpacity>
-    
-    <TouchableOpacity onPress={()=> openwebsite('https://reactnative.dev/docs/getting-started')}>
-    <Text style= {styles.linktext}> Visit </Text>
-    </TouchableOpacity>
+    <View>
+      <Pressable onPress={toggleDropdown}>
+        <Text style={{fontSize: 20}}>
+          {isOpen ? 'Close Dropdown' : 'Open Dropdown'}
+        </Text>
+      </Pressable>
+      {isOpen && (
+        <View style={{backgroundColor: 'lightgray', padding: 10}}>
+          <Text>close</Text>
+        </View>
+      )}
     </View>
-    
-    </View>
-  </ScrollView>
-    
-      
-
-
-  )
+  );
 };
 
-
-
-const styles = StyleSheet.create({
-  Image:{
-    width:400,
-    height:400,
-    borderRadius:10,
-
-  },
- 
-  
- reactview:{
-  backgroundColor:'#e9967a'
-    },
-    touch:{
-      flexDirection:'row',
-      alignItems:'center',
-      justifyContent:'space-evenly',
-    },
-    
-  
-    heading:{
-      fontSize:20,
-      fontWeight:'bold',
-      paddingLeft:20,
-      paddingBottom:20,
-      
-    },
-    headview:{
-      paddingLeft:25,
-  color:'black',
-  fontSize:16,
-    },
-    ans:{
-      paddingLeft:25,
-      paddingBottom:20,
-      color:'black',
-    },
-    imgview:{
-      
-      
-      alignItems:'center',
-      paddingBottom:20,
-      paddingtop:20,
-      margin:8,
-    },
-    linktext:{
-      color:'black',
-      fontSize:16,
-     
-  
-  
-    },
-
-}
-);
-
-export default App;
+export default Dropdown;
