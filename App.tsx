7c88e7bd33a060f267ea845504cd80da390f65c3
Kamalis8 <kamali.s@optisolbusiness.com>
2023-10-17 17:39:57 +0530

    components: imagebackground/text/button

--- App.tsx
+++ App.tsx
@@ -5,36 +5,40 @@
  *
  * @format
  */
-import React, {useState} from 'react';
-import {View, Switch, Text, StyleSheet, StatusBar} from 'react-native';
+import React from 'react';
+import {Text, ImageBackground, StyleSheet, Button, Alert} from 'react-native';
 
 const App = () => {
-  StatusBar.setHidden(true);
-
-  const [isEnabled, setIsEnabled] = useState(false);
-
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  function handleClick() {
+    Alert('Button Clicked!');
+  }
 
   return (
-    <View>
-      <Text style={styles.text}>Airplane Mode:</Text>
-      <Switch
-        trackColor={{false: 'grey', true: 'green'}}
-        thumbColor={isEnabled ? 'white' : 'grey'}
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://images.pexels.com/photos/5077039/pexels-photo-5077039.jpeg?auto=compress&cs=tinysrgb&w=600',
+      }}
+      style={styles.background}
+      resizeMode="cover">
+      <Text style={styles.text}>Welcome</Text>
+
+      <Button title="Click me" onPress={handleClick} />
+    </ImageBackground>
   );
 };
+
 const styles = StyleSheet.create({
-  text: {
-    color: 'black',
-    fontsize: 30,
-    fontweight: 'bold',
+  background: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  statusbar: {
-    backgroundColor: 'yellow',
+  text: {
+    color: 'yellow',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
+
 export default App;
