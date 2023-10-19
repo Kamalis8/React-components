21771ee53d317ba2920505698e2fe039ceab1cf6
Kamalis8 <kamali.s@optisolbusiness.com>
2023-10-19 17:30:22 +0530

    components: model

--- App.tsx
+++ App.tsx
@@ -1,132 +1,122 @@
-/* eslint-disable @typescript-eslint/no-shadow */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
  * @format
  */
-import React from 'react';
-import {View, Text, FlatList, Image, StyleSheet} from 'react-native';
-
-const data = [
-  {
-    id: 1,
-    title: 'Great Wall of China',
-    about:
-      'The Great Wall of China is an ancient defensive fortification that spans over 13,000 miles, constructed to protect China from invasions by nomadic tribes. It stands as an enduring symbol of China rich history and cultural heritage.',
-    image:
-      'https://images.pexels.com/photos/10952316/pexels-photo-10952316.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 2,
-    title: 'Chichen Itza',
-    about:
-      'Chichen Itza is a renowned archaeological site located in Mexico Yucatan Peninsula, known for its well-preserved Mayan ruins including the iconic El Castillo pyramid. It served as a significant ancient city and ceremonial center showcasing advanced architectural and astronomical knowledge of the Mayan civilization.',
-    image:
-      'https://images.pexels.com/photos/3290068/pexels-photo-3290068.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 3,
-    title: 'Petra',
-    about:
-      'Petra, also known as the "Rose City," is an archaeological marvel in Jordan, renowned for its intricate rock-cut architecture, including the iconic Treasury. This ancient Nabatean city, nestled within rugged desert terrain, stands as a UNESCO World Heritage site and a testament to ancient engineering prowess.',
-    image:
-      'https://images.pexels.com/photos/720254/pexels-photo-720254.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 4,
-    title: 'Machu Picchu',
-    about:
-      'Machu Picchu is an ancient Incan citadel perched high in the Andes Mountains of Peru, known for its extraordinary stone architecture and breathtaking panoramic views. This UNESCO World Heritage site is a testament to Incan engineering and a symbol of their advanced civilization.',
-    image:
-      'https://images.pexels.com/photos/2929906/pexels-photo-2929906.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 5,
-    title: 'Christ the Redeemer',
-    about:
-      'Christ the Redeemer is an iconic statue of Jesus Christ, standing atop the Corcovado mountain in Rio de Janeiro, Brazil. It is a globally recognized symbol of Christianity and a remarkable feat of engineering and artistry.',
-    image:
-      'https://images.pexels.com/photos/13911606/pexels-photo-13911606.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 6,
-    title: 'Colosseum',
-    about:
-      'The Colosseum, also known as the Flavian Amphitheatre, is an ancient Roman arena located in the heart of Rome, Italy. Built in the 1st century AD, it once held up to 80,000 spectators for gladiatorial contests, animal hunts, and other public spectacles.',
-    image:
-      'https://images.pexels.com/photos/2225439/pexels-photo-2225439.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-  {
-    id: 7,
-    title: 'Taj Mahal',
-    about:
-      'The Taj Mahal is an exquisite white marble mausoleum in Agra, India, built by Mughal Emperor Shah Jahan in memory of his beloved wife Mumtaz Mahal. Renowned for its architectural beauty and symmetry, it stands as an enduring symbol of love and is a UNESCO World Heritage site.',
-    image:
-      'https://images.pexels.com/photos/6827267/pexels-photo-6827267.jpeg?auto=compress&cs=tinysrgb&w=400',
-  },
-];
-
-const renderItem = ({item}) => {
-  //console.log('item', item);
-  return (
-    <>
-      <View style={styles.itemContainer}>
-        <Image source={{uri: item.image}} style={styles.image} />
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.text}>{item.about}</Text>
-      </View>
-    </>
-  );
-};
+import React, {useState} from 'react';
+import {
+  Modal,
+  Text,
+  TouchableHighlight,
+  View,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <>
-      <View style={styles.heading}>
-        <Text style={styles.headingText}>Wonders of World</Text>
-      </View>
-      <View>
-        <FlatList data={data} renderItem={renderItem} />
-      </View>
-    </>
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View>
+          <View>
+            <View style={styles.container}>
+              <Text style={styles.heading}> Deactivate Account </Text>
+              <Text style={styles.text}>
+                Are you sure u want to deactivate your account? All your date
+                will be permentaly removed.This action cannot be undone.
+              </Text>
+              <View style={styles.button}>
+                <TouchableOpacity style={styles.can}>
+                  <Text style={styles.textcan}>Cancel</Text>
+                </TouchableOpacity>
+                <View style={styles.space} />
+                <TouchableOpacity style={styles.deactive}>
+                  <Text style={styles.textdeactive}>Deactivate</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableHighlight
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Text>Close</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        onPress={() => {
+          setModalVisible(true);
+        }}>
+        <Text style={styles.heading}>Click here to Deactivate</Text>
+      </TouchableHighlight>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
   },
-
-  itemContainer: {
-    alignItems: 'center',
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  image: {
-    width: 400,
-    height: 400,
-    marginRight: 10,
-    borderRadius: 10,
+  heading: {
+    fontsize: 60,
+    fontweight: 'bold',
+    color: 'black',
+    marginTop: 10,
+    paddingBottom: 10,
+    marginLeft: 10,
+    paddingLeft: 8,
   },
   text: {
-    fontSize: 20,
+    //marginLeft: 15,
+    paddingLeft: 20,
   },
-  title: {
-    fontSize: 20,
+  deactive: {
+    backgroundColor: 'red',
+  },
+  can: {
+    backgroundColor: 'white',
+  },
+  textcan: {
+    fontSize: 15,
     fontWeight: 'bold',
+    color: 'grey',
+    borderWidth: 1,
+    borderColor: 'grey',
+    paddingTop: 12,
+    paddingLeft: 16,
+    paddingRight: 16,
+    margin: 8,
+    paddingBottom: 12,
   },
-
-  heading: {
+  textdeactive: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+    margin: 8,
+    padding: 6,
+  },
+  button: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingRight: 10,
   },
-  headingText: {
-    fontSize: 30,
-    color: 'black',
-    fontWeight: 'bold',
+  space: {
+    width: 20,
   },
 });
 
